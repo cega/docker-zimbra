6492f6e448adea332f6ecda084e55d6b964affc9
Linux User <Ernad husremovic>
2015-09-03 16:49:37 +0200

    container name

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,6 +1,10 @@
+#!/bin/bash
+
+CONTAINER_NAME=zimbra-1
 ZIMBRA_TMP_VOL=/data/zimbra/tmp_data
 ZIMBRA_VOL=/data/zimbra/opt_zimbra
 docker run \
+ --name $CONTAINER_NAME \
  -p 25:25 -p 80:80 -p 456:456 -p 587:587 -p 110:110 \
  -p 143:143 -p 993:993 -p 995:995 -p 443:443 \
  -p 8080:8080 -p 8443:8443 -p 7071:7071 -p 9071:9071 \
