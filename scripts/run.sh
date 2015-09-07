573dfddca662e554fdaa4871a771e7192d3b77e6
Linux User <Ernad husremovic>
2015-09-07 14:38:38 +0200

    zimbra install config 8.0, 8.6

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-CONTAINER_NAME=zimbra-1
+CONTAINER_NAME=zimbra-86
 ZIMBRA_TMP_VOL=/data/zimbra/tmp_data
 ZIMBRA_VOL=/data/zimbra/opt_zimbra
 ZIMBRA_VOL_DATA=/data/zimbra/opt_zimbra_data
@@ -9,11 +9,19 @@ ZIMBRA_VOL_CONF=/data/zimbra/opt_zimbra_conf
 ZIMBRA_VOL_DB=/data/zimbra/opt_zimbra_db
 ZIMBRA_VOL_STORE=/data/zimbra/opt_zimbra_store
 
-PASSWORD=Zimbra2015
+ZIMBRA_PASSWORD=${ZIMBRA_PASSWORD:-Zimbra2015}
+ZIMBRA_HOST=${ZIMBRA_HOST:-zimbra-82}
+ZIMBRA_DOMAIN=${ZIMBRA_DOMAIN:-bring.out.ba}
+
+#ZIMBRA_VER=8.0.9_GA
+#ZIMBRA_TGZ=zcs-8.0.9_GA_6191.UBUNTU14_64.20141103151539
+
 DNS_FORWARD_1=192.168.168.20
 DNS_FORWARD_2=192.168.168.106
-ZIMBRA_HOST=zimbra
-ZIMBRA_DOMAIN=local
+
+ZIMBRA_CLEANUP=no
+
+
 
 docker run \
  --name $CONTAINER_NAME \
@@ -30,10 +38,12 @@ docker run \
  -v $ZIMBRA_VOL_STORE:/opt/zimbra/store \
  --dns 127.0.0.1 \
  -it \
+ -e ZIMBRA_PASSWORD=$ZIMBRA_PASSWORD \
  -e ZIMBRA_HOST=$ZIMBRA_HOST \
  -e ZIMBRA_DOMAIN=$ZIMBRA_DOMAIN \
  -e ZIMBRA_CLEANUP=$ZIMBRA_CLEANUP \
- -e PASSWORD=$PASSWORD \
  -e DNS_FORWARD_1=$DNS_FORWARD_1 \
  -e DNS_FORWARD_2=$DNS_FORWARD_2 \
+ -e ZIMBRA_VER=$ZIMBRA_VER \
+ -e ZIMBRA_TGZ=$ZIMBRA_TGZ \
  zimbra $1
