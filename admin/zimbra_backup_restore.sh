0bf72c99bbea866485d2888218754562b92ebb09
Linux User <Ernad husremovic>
2015-09-09 15:50:31 +0200

    zmcontrol restart

--- admin/zimbra_backup_restore.sh
+++ admin/zimbra_backup_restore.sh
@@ -6,11 +6,11 @@ if [[ $RESTORE_MOD == yes_i_want_restore ]]
 then
 
  cd /tmp_data/backup
- su zimbra  -c "PATH=\$PATH:/opt/zimbra/bin &&
+ su zimbra  -c "PATH=\$PATH:/opt/zimbra/bin && zmcontrol stop &&
 cd /tmp_data/backup && /admin/restore_ldap.sh &&
 /admin/restore_mysql.sh &&
 cd /opt/zimbra && 
-tar xvf /tmp_data/backup/store.tar.gz"
+tar xvf /tmp_data/backup/store.tar.gz && zmcontrol restart"
  /opt/zimbra/libexec/zmfixperms -extended  -verbose
 else
   echo "RESTORE_MOD envar not set !"
