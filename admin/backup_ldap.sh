bf502c775b999166adc17738164561241a10bf6e
Linux User <Ernad husremovic>
2015-09-15 15:37:26 +0200

    ldap start prije ldap backupa

--- admin/backup_ldap.sh
+++ admin/backup_ldap.sh
@@ -8,6 +8,9 @@ else
   BACKUP_DIR=./ldap
 fi
 
+echo LDAP backup to $BACKUP_DIR
+/opt/zimbra/bin/ldap start
+
 /opt/zimbra/libexec/zmslapcat -c $BACKUP_DIR
 /opt/zimbra/libexec/zmslapcat $BACKUP_DIR
 
