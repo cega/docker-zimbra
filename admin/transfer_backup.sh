4fde53abd45bc80855ff8894e07b0c1bf33c2aa9
Linux User <Ernad husremovic>
2015-09-16 16:53:09 +0200

    slash

--- admin/transfer_backup.sh
+++ admin/transfer_backup.sh
@@ -7,5 +7,5 @@ SRC=$1
 
 BACKUP_BASE=/tmp_data/backup
 
-rsync -avz --delete root@$SRC:$BACKUP_BASE $BACKUP_BASE
+rsync -avz --delete root@$SRC:$BACKUP_BASE/ $BACKUP_BASE/
 chown zimbra:zimbra -R $BACKUP_BASE
