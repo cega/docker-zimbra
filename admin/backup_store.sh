aba61b4a9192eb704aa2cfb3403ff2cc595627ff
Linux User <Ernad husremovic>
2015-09-16 11:01:59 +0200

    backup store

---
+++ admin/backup_store.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+echo run as root !
+
+BACKUP_ROOT=/tmp_data/backup
+mkdir -p BACKUP_ROOT
+
+chown zimbra:zimbra -R $BACKUP_ROOT
+
+cd /opt/zimbra
+
+tar -czvf $BACKUP_ROOT/store.tar.gz store
+
