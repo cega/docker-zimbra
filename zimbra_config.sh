7adf39169764da7aca2799d16c0aa95e7f155f5a
Linux User <Ernad husremovic>
2015-09-17 13:38:57 +0200

    ispitaj ENVARS

--- zimbra_config.sh
+++ zimbra_config.sh
@@ -4,8 +4,14 @@ export PATH=$PATH:/opt/zimbra/bin
 
 echo zimbra config
 
+if [[ "$ZIMBRA_HOST" == "zimbra-82" ]] && [[ "$ZIMBRA_DOMAIN" = "bring.out.ba" ]] ; then
+
 zmprov mcf zimbraMtaSmtpdClientPortLogging yes
 zmprov mcf zimbraMtaMyHostname zimbra.bring.out.ba
 zmprov mcf zimbraMtaRelayHost smtp-ext-gw.bring.out.ba
 
 postfix reload
+
+else
+   echo nothing to configure
+fi
